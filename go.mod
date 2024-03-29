532498b672b5f5e5088f0103c5cddcb1b16d5ec9
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2024-03-29 12:02:39 +0700

    feat: init orchestration using docker compose & init setup with prometheus & grafana

--- go.mod
+++ go.mod
@@ -6,23 +6,22 @@ require (
 	github.com/aws/aws-sdk-go v1.51.2
 	github.com/gin-gonic/gin v1.9.1
 	github.com/google/uuid v1.6.0
-	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jackc/pgx/v5 v5.5.5
 )
 
 require (
-	github.com/cockroachdb/apd v1.1.0 // indirect
-	github.com/gofrs/uuid v4.4.0+incompatible // indirect
-	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
-	github.com/shopspring/decimal v1.3.1 // indirect
-	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
 )
 
 require (
@@ -40,11 +39,11 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
+	github.com/prometheus/client_golang v1.19.0
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.7.0 // indirect
--- router/router.go
+++ router/router.go
@@ -1,11 +1,25 @@
 package router
 
 import (
+	"log"
 	"social-media-app/controllers"
 	"social-media-app/middleware"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	requestHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "http_request_duration_seconds",
+		Help:    "Histogram of request duration.",
+		Buckets: prometheus.LinearBuckets(1, 1, 10),
+	}, []string{"path", "method", "status"})
 )
 
 func StartApp(DB *pgxpool.Pool) *gin.Engine {
@@ -17,13 +31,32 @@ func StartApp(DB *pgxpool.Pool) *gin.Engine {
 
 	userAccount := router.Group("/v1/user")
 	{
-		userAccount.POST("/register", middleware.RegisterValidator(), controllers.UserRegister)
-		userAccount.POST("/login", middleware.AuthValidator(), controllers.UserLogin)
+		userAccount.POST("/register", wrapHandlerWithMetrics("v1/user/register", "POST", controllers.UserRegister, middleware.RegisterValidator()))
+		userAccount.POST("/login", wrapHandlerWithMetrics("v1/user/login", "POST", controllers.UserLogin, middleware.AuthValidator()))
 	}
 
 	router.POST("/v1/image", middleware.Authentication(), controllers.CreateUploadImage)
 
 	router.GET("/health-check", controllers.ServerCheck)
 
+	router.GET("/metrics", gin.WrapH(promhttp.Handler()))
+
 	return router
+}
+
+func wrapHandlerWithMetrics(path, method string, handler gin.HandlerFunc, middleware ...gin.HandlerFunc) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		startTime := time.Now()
+
+		// Chain middleware functions
+		for _, m := range middleware {
+			m(c)
+		}
+
+		handler(c)
+
+		duration := time.Since(startTime).Seconds()
+		log.Println(path, method, strconv.Itoa(c.Writer.Status()))
+		requestHistogram.WithLabelValues(path, method, strconv.Itoa(c.Writer.Status())).Observe(duration)
+	}
 }
